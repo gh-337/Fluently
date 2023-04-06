d355f37a9d9b18c296799a30b10d189304bb8735
gh-337 <neyro337@gmail.com>
2023-04-06 21:12:24 +0300

    test_019

--- app/src/main/java/com/example/fluently/LevelActivity.kt
+++ app/src/main/java/com/example/fluently/LevelActivity.kt
@@ -1,19 +1,59 @@
 package com.example.fluently
 
-import android.content.Intent
+import android.R
+import android.app.ProgressDialog
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.example.fluently.databinding.ActivityEasyLevelBinding
 import com.example.fluently.databinding.ActivityLevelBinding
+import com.google.android.material.transition.MaterialContainerTransform.ProgressThresholds
+import com.google.firebase.storage.FirebaseStorage
+import java.io.File
 
 class LevelActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLevelBinding
+ lateinit var binding: ActivityLevelBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityLevelBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.getImage.setOnClickListener{
+
+            val progressDialog = ProgressDialog(this)
+            progressDialog.setMessage("Fetching image....")
+            progressDialog.setCancelable(false)
+            progressDialog.show()
+
+
+            val imageName = binding.etImageId.text.toString()
+            val storageRef  = FirebaseStorage.getInstance().reference.child("images/$imageName.jpg")
+
+            val localfile = File.createTempFile("tempImage", "jpg")
+            storageRef.getFile(localfile).addOnSuccessListener {
+
+                if(progressDialog.isShowing)
+                    progressDialog.dismiss()
+
+
+                val bitmap = BitmapFactory.decodeFile(localfile.absolutePath)
+                binding.imageView.setImageBitmap(bitmap)
+
+
+            }.addOnFailureListener{
+
+                if(progressDialog.isShowing)
+                    progressDialog.dismiss()
+
+                Toast.makeText(this,"Failed to retrieve thr image",Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+
         //Toast.makeText(this, intent.extras?.getString("Lang").toString(), Toast.LENGTH_SHORT).show()
         Toast.makeText(this, intent.extras?.getString(Const.LANGUAGE), Toast.LENGTH_SHORT).show()
         Toast.makeText(this, intent.extras?.getString(Const.DIFFICULT), Toast.LENGTH_SHORT).show()
