a4bfb2498408d4936639d07f98291232807dd8ea
PavOn1 <98577965+PavOn1@users.noreply.github.com>
2023-04-13 04:33:47 +0300

    test_42

--- app/src/main/java/com/example/fluently/LevelActivity.kt
+++ app/src/main/java/com/example/fluently/LevelActivity.kt
@@ -1,8 +1,11 @@
 package com.example.fluently
 
+import android.app.Dialog
 import android.app.ProgressDialog
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -31,7 +34,20 @@ class LevelActivity : AppCompatActivity() {
         val npp=1
         var id = "";
         getDataAct(language, difficult, level, npp)
+        binding.btn.setOnClickListener {
+            val dialogBinding = layoutInflater.inflate(R.layout.my_custom_dialog,null)
 
+            val myDialog = Dialog(this)
+            myDialog.setContentView(dialogBinding)
+
+            myDialog.setCancelable(true)
+            myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            myDialog.show()
+
+
+
+
+        }
 
 
 
